0ae3236c033eb97e9d0cba3590e7a8e3477c827e
Pablo <pablito@me.com>
2024-08-12 19:13:07 +0100

    chore: refactor of names.

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -2,30 +2,26 @@ import {useEffect, useState} from 'react';
 import './Matrix.css'; // Import CSS file for styling
 
 const Matrix = () => {
-  const [focusRow, setFocusRow] = useState(null)
+  const [hoveredRow, setHoveredRow] = useState(null)
 
   const handleMouseEnterRow = (index) => {
-    console.log(`*** setting row: ${index}`)
-    setFocusRow(index);
+    setHoveredRow(index);
   };
 
   const handleMouseExitRow = () => {
-    console.log(`*** setting row: null`)
-    setFocusRow(null);
+    setHoveredRow(null);
   };
 
   return (
       <div className="rows-container">
         {[...Array(3).keys()].map((index) => (
-            <Row
+            <MatrixRow
                 key={`row-${index}`}
                 className={`row`}
                 rowIndex={index}
-                focusRow={focusRow}
+                hoveredRow={hoveredRow}
                 handleMouseEnterRow={handleMouseEnterRow}
                 handleMouseExitRow={handleMouseExitRow}
-                // onMouseEnter={() => handleMouseEnterRow(index)}
-                // onMouseLeave={handleMouseExitRow}
             />
         ))}
       </div>
@@ -33,21 +29,20 @@ const Matrix = () => {
 }
 
 // eslint-disable-next-line react/prop-types
-const Row = ({rowIndex, focusRow, handleMouseEnterRow, handleMouseExitRow}) => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+const MatrixRow = ({rowIndex, hoveredRow, handleMouseEnterRow, handleMouseExitRow}) => {
+  const [hoveredColumn, setHoveredColumn] = useState(null);
 
   const handleMouseEnterCol = (index) => {
-    console.log(`setting col: ${index}`)
-    setHoveredIndex(index);
+    setHoveredColumn(index);
   };
 
   const handleMouseExitCol = () => {
-    setHoveredIndex(null);
+    setHoveredColumn(null);
   };
 
   useEffect(() => {
 
-    console.log(`focus - col: ${hoveredIndex}, row: ${focusRow}`)
+    console.log(`focus - col: ${hoveredColumn}, row: ${hoveredRow}`)
   })
 
   return (
@@ -55,13 +50,13 @@ const Row = ({rowIndex, focusRow, handleMouseEnterRow, handleMouseExitRow}) => {
            onMouseEnter={() => handleMouseEnterRow(rowIndex)}
            onMouseLeave={handleMouseExitRow}>
       {[...Array(4).keys()].map((index) => (
-        <Panel
+        <MatrixPanel
           key={index}
           rowIndex={rowIndex}
           colIndex={index}
-          focusRow={focusRow}
-          isHovered={hoveredIndex === index}
-          hoveredIndex={hoveredIndex}
+          hoveredRow={hoveredRow}
+          isHovered={hoveredColumn === index}
+          hoveredColumn={hoveredColumn}
           onMouseEnterCol={handleMouseEnterCol}
           onMouseExitCol={handleMouseExitCol}
         />
@@ -73,10 +68,10 @@ const Row = ({rowIndex, focusRow, handleMouseEnterRow, handleMouseExitRow}) => {
 
 
 // eslint-disable-next-line react/prop-types
-const Panel = ({ rowIndex, colIndex, focusRow, isHovered, onMouseEnterCol, onMouseExitCol, hoveredIndex }) => {
+const MatrixPanel = ({ rowIndex, colIndex, hoveredRow, isHovered, onMouseEnterCol, onMouseExitCol, hoveredColumn }) => {
   return (
     <div
-      className={`panel ${isHovered ? 'active' : hoveredIndex === null ? 'passive' : ''} ${focusRow === rowIndex ? "active-row" : focusRow === null ?  "passive-row" : ""}`}
+      className={`panel ${isHovered ? 'active' : hoveredColumn === null ? 'passive' : ''} ${hoveredRow === rowIndex ? "active-row" : hoveredRow === null ?  "passive-row" : ""}`}
       onMouseEnter={() => onMouseEnterCol(colIndex)}
       onMouseLeave={onMouseExitCol}
     >
@@ -87,4 +82,4 @@ const Panel = ({ rowIndex, colIndex, focusRow, isHovered, onMouseEnterCol, onMou
   );
 };
 
-export default Matrix;
+export default Matrix
